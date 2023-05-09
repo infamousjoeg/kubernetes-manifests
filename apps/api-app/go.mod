c225eeda6327f50fdc57f28311124202291f1a83
Joe Garcia <joe@joe-garcia.com>
2023-05-09 10:26:01 -0400

    Updated to use RetrieveBatchSecrets

--- apps/api-app/go.mod
+++ apps/api-app/go.mod
@@ -2,11 +2,18 @@ module github.com/infamousjoeg/kubernetes-manifests/apps/api-app
 
 go 1.17
 
-require github.com/cyberark/conjur-api-go v0.9.0
+require github.com/cyberark/conjur-api-go v0.11.0
 
 require (
+	github.com/alessio/shellescape v1.4.1 // indirect
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d // indirect
+	github.com/danieljoos/wincred v1.1.2 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86 // indirect
+	github.com/zalando/go-keyring v0.2.2 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- apps/api-app/main.go
+++ apps/api-app/main.go
@@ -1,40 +1,127 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/cyberark/conjur-api-go/conjurapi"
+	_ "github.com/lib/pq"
+	"github.com/olekukonko/tablewriter"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Get variable path from environment variable
-	conjurUserObject := os.Getenv("CONJUR_USER_OBJECT")
-	conjurPassObject := os.Getenv("CONJUR_PASS_OBJECT")
+	// Get variables from environment variable
+	// CONJUR_SECRETS=ADDRESS;DATABASE_NAME;USERNAME;PASSWORD
+	// Replace above with your own variables
+	conjurSecrets := os.Getenv("CONJUR_SECRETS")
+	if conjurSecrets == "" {
+		log.Fatal("CONJUR_SECRETS environment variable not set")
+	}
 
+	// Split conjurSecrets on semi-colon into a slice of strings
+	conjurSecretsSlice := strings.Split(conjurSecrets, ";")
+
+	// Load Conjur configuration from environment variables
 	config, err := conjurapi.LoadConfig()
 	if err != nil {
 		log.Fatalf("Error loading configuration: %s", err)
 	}
 
+	// Create a new Conjur client
 	conjurClient, err := conjurapi.NewClientFromEnvironment(config)
 	if err != nil {
 		log.Fatalf("Error creating client: %s", err)
 	}
 
-	userObjectValue, err := conjurClient.RetrieveSecret(conjurUserObject)
+	// Retrieve secret(s) from Conjur
+	secretsValueMap, err := conjurClient.RetrieveBatchSecrets(conjurSecretsSlice)
 	if err != nil {
-		log.Fatalf("Error retrieving %s: %s", userObjectValue, err)
+		log.Fatalf("Error retrieving secret(s): %s", err)
 	}
 
-	passObjectValue, err := conjurClient.RetrieveSecret(conjurPassObject)
+	fmt.Fprintf(w, "API Example\n==========\nHost: %s\nDatabase Name: %s\nUser: %s\nPassword: %s",
+		string(secretsValueMap[conjurSecretsSlice[0]]), // Host
+		string(secretsValueMap[conjurSecretsSlice[1]]), // Database Name
+		string(secretsValueMap[conjurSecretsSlice[2]]), // Username
+		string(secretsValueMap[conjurSecretsSlice[3]])) // Password
+
+	// Connect to the PostgreSQL database
+	connStr := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",
+		string(secretsValueMap[conjurSecretsSlice[0]]), // Host
+		string(secretsValueMap[conjurSecretsSlice[1]]), // Database Name
+		string(secretsValueMap[conjurSecretsSlice[2]]), // Username
+		string(secretsValueMap[conjurSecretsSlice[3]])) // Password
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatalf("Error retrieving %s: %s", passObjectValue, err)
+		fmt.Fprintf(w, "\nFailed to connect to database: %v", err)
+		return
+	}
+	defer db.Close()
+
+	// Query the 'customers' table
+	rows, err := db.Query("SELECT * FROM customers")
+	if err != nil {
+		fmt.Fprintf(w, "\nFailed to execute query: %v", err)
+		return
+	}
+	defer rows.Close()
+
+	// Get column descriptions
+	columns, err := rows.ColumnTypes()
+	if err != nil {
+		fmt.Fprintf(w, "\nFailed to get column types: %v", err)
+		return
 	}
 
-	fmt.Fprintf(w, "API Example\n==========\nUser: %s\nPassword: %s", string(userObjectValue), string(passObjectValue))
+	// Initialize tablewriter
+	table := tablewriter.NewWriter(w)
+	header := make([]string, len(columns))
+	for i, column := range columns {
+		header[i] = column.Name()
+	}
+	table.SetHeader(header)
+	table.SetRowLine(true)
+	table.SetAutoWrapText(false)
+
+	// Iterate through rows and write to table
+	for rows.Next() {
+		// Prepare a slice of interface{} to hold the scanned values
+		values := make([]interface{}, len(columns))
+		pointers := make([]interface{}, len(columns))
+
+		for i := range values {
+			pointers[i] = &values[i]
+		}
+
+		// Scan the row data into the values slice
+		err := rows.Scan(pointers...)
+		if err != nil {
+			fmt.Fprintf(w, "\nFailed to scan row: %v", err)
+			return
+		}
+
+		// Add the row to the table
+		row := make([]string, len(columns))
+		for i, val := range values {
+			row[i] = fmt.Sprintf("%v", val)
+		}
+		table.Append(row)
+	}
+
+	// Check for any errors encountered during iteration
+	err = rows.Err()
+	if err != nil {
+		fmt.Fprintf(w, "\nFailed to iterate over rows: %v", err)
+		return
+	}
+
+	// Render the table
+	fmt.Fprint(w, "\n\nResults from the database:\n")
+	table.Render()
 }
 
 func main() {
